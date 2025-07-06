84726a124527767999d0392a9bc8399706f71aff
Lucas Machado <lmachado72@outlook.com>
2025-07-06 20:12:14 -0300

    feat: added categories schema, seed and push categories to db, trpc routers refactored, categories prefetched and categories component created

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,24 @@
-import { HydrateClient, trpc } from "@/trpc/server"
-import { Suspense } from "react"
+import { HomeView } from "@/modules/home/ui/views/home-view";
+import { HydrateClient, trpc } from "@/trpc/server";
 
-import { ErrorBoundary } from "react-error-boundary"
-import { PageClient } from "./client"
+export const dynamic = 'force-dynamic'
 
-export default async function Home() {
-    void trpc.hello.prefetch({ text: 'Lucas' })
+interface PageProps {
+    searchParams: Promise<{
+        categoryId?: string;
+    }>
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+    const { categoryId } = await searchParams
+
+    void trpc.categories.getMany.prefetch()
 
     return (
         <HydrateClient>
-            <Suspense fallback={<p>Loading...</p>}>
-                <ErrorBoundary fallback={<p>Error...</p>}>
-                    <PageClient />
-                </ErrorBoundary>
-            </Suspense>
+            <HomeView categoryId={categoryId} />
         </HydrateClient>
     )
 }
+
+export default Page
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,22 +1,9 @@
-// Define um conjunto de rotas do tRPC
+import { categoriesRouter } from '@/modules/categories/server/procedures'
+import { createTRPCRouter } from '../init'
 
-import { z } from 'zod'
-import { createTRPCRouter, protectedProcedure } from '../init'
-
-// Cria uma rota chamada hello que espera uma string como input e retorna um greeting.
 // Tipagem automática com Zod + tRPC
 export const appRouter = createTRPCRouter({
-    hello: protectedProcedure.input(
-        z.object({
-            text: z.string(),
-        }),
-    ).query((opts) => {
-        console.log({ dbUser: opts.ctx.user })
-
-        return {
-            greeting: `Hello ${opts.input.text}`,
-        }
-    })
+    categories: categoriesRouter
 })
 
 // Exporta o tipo do roteador para o cliente poder saber quais rotas estão disponíveis com tipos corretos
